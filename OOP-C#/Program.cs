7a13dc64181fadec5661854e6882ea02ec459443
Al Momen <almomen535@gmail.com>
2025-03-07 11:01:56 +0600

    Basic of oop. class and objects

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace OOP_C_
 {
+    class Person
+    {
+        public string name;
+        public int age;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person p1 = new Person();
+            p1.name = "Al Momen";
+            p1.age = 24;
+            Console.WriteLine("Person1:");
+            Console.WriteLine($"Name: {p1.name},Age: {p1.age}");
+
+
+            Person p2 = new Person();
+            p2.name = "Samiul Islam";
+            p2.age = 26;
+            Console.WriteLine("Person2: ");
+            Console.WriteLine($"Name: {p2.name}, Age: {p2.age}");
         }
     }
 }
