4ac16690b9fb6d8ecd3deb76c686f7a32fe94754
Al Momen <almomen535@gmail.com>
2025-03-07 11:06:04 +0600

    Encapsulation using getter and setter

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace OOP_C_
 {
-    internal class Program
+    class Person
+    {
+
+        public string name;
+        private int age;
+
+
+
+        public void SetName(string n)
+        {
+            name = n;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetAge(int a)
+        {
+            age = a;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        // Method-> DisplayInfo
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {GetName()} and Age: {GetAge()}\n");
+        }
+
+
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Object-> p1
+            Person p1 = new Person();
+            p1.name = "Momen";
+            p1.SetName("Al Momen");
+            p1.SetAge(33);
+            p1.DisplayInfo();
+
+
         }
     }
+
 }
