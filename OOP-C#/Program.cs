05fcec863a1b287251f0988f9246c56341a48ea3
Al Momen <almomen535@gmail.com>
2025-03-08 11:53:40 +0600

    using this and readonly

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace OOP_C_
 {
-    internal class Program
+
+    public class Student
+    {
+        public string name;
+        public readonly int age;
+
+
+        public Student(int age)
+        {
+            this.age = age;
+        }
+    }
+    public class Myclass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Student p1 = new Student(29);
+
+            Console.WriteLine($"Age= {p1.age}");
         }
     }
 }
