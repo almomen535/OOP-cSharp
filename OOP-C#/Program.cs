b5003fbcf42f4d9cfdfdd1f0d22f6deb7aefc2b7
Al Momen <almomen535@gmail.com>
2025-03-11 14:16:08 +0600

    Added Author class

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace OOP_C_
+﻿using System.Security.Cryptography;
+
+namespace OOP_C_
 {
+
+
+    class Author
+    {
+       public string name;
+       public string email;
+       public string nationality;
+
+        public void GetDetails()
+        {
+            Console.WriteLine($"Author: {name}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Nationality: {nationality}");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Momen!");
+
+            Author p1 = new Author();
+            p1.name= "J.K. Rowling";
+            p1.email = "jkrowling@example.com";
+            p1.nationality = "British";
+            p1.GetDetails();
+
+
         }
     }
 }
