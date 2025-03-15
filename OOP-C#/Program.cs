ac1f3acdec375a95510391a482db8db720e011cb
Al Momen <almomen535@gmail.com>
2025-03-15 13:14:07 +0600

    Corrected1: Add property, used constructor, added linking

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿
+using System.Globalization;
 
 namespace OOP_C_
 {
@@ -6,48 +7,68 @@ namespace OOP_C_
 
     class Author
     {
-        public string name;
-        public string email;
-        public string nationality;
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Nationality { get; set; }
+
+
+        public Author(string name, string email, string nationality)
+        {
+            Name = name;
+            Email = email;
+            Nationality = nationality;
+        }
 
         public string GetDetails()
         {
 
-            return $" Author:{name}\n Email:{email}\n Nationality: {nationality}";
-
+            return $" Author:{Name}\n Email:{Email}\n Nationality: {Nationality}";
 
         }
     }
 
     class Book
     {
-        public string title;
-        public string ISBN;
-        public double price;
+        public string Title { get; set; }
+        public string ISBN {  get; set; }
+        public double Price { get; set; }
+        public Author BookAuthor { get; set; } //BookAuthor is a reference variable. It holds a reference (or pointer) to an object of the Author class.
+
+
+        public Book(string title, string isbn, double price, Author bookauthor)
+        {
+            Title = title;
+            ISBN = isbn;
+            Price = price;
+            BookAuthor = bookauthor;
+           
+        }
 
         public string GetBookInfo()
         {
-            return $" Book Title: {title}\n ISBN:{ISBN}\n Price: {price}";
+            return $" Book Title: {Title}\n ISBN:{ISBN}\n Price: ${Price}\n{BookAuthor.GetDetails()}\n";
         }
 
+    }
 
         internal class Program
         {
             static void Main(string[] args)
             {
 
-                Author p1 = new Author();
-                p1.name = "J.K. Rowling";
-                p1.email = "jkrowling@example.com";
-                p1.nationality = "British";
-                Console.WriteLine(p1.GetDetails());
+                Author author1 = new Author("J.K. Rowling", "jkrowling@example.com", "British");
+                Author author2 = new Author("George Orwell", "george@example.com", "British");
 
-                Book p2 = new Book();
-                p2.title = "Harry Potter";
-                p2.ISBN = "123-4567890123";
-                p2.price = 19.99;
-                Console.WriteLine(p2.GetBookInfo());
-            }
+                Book book1 = new Book("Harry Potter", "123-4567890123", 19.99,author1);
+                Book book2 = new Book("Animal Farm", "345-6789012345", 12.99, author2);
+                Book book3 = new Book("Galaxy", "234-5678901234", 14.99, author1);
+
+                Console.WriteLine(book1.GetBookInfo());
+
+                Console.WriteLine(book2.GetBookInfo());
+                Console.WriteLine(book3.GetBookInfo());
+
+        }
         }
     }
-}
+
