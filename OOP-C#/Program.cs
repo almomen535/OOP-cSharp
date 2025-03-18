f653bc39b537c16530911d95ca26c0178d4f52f8
Al Momen <almomen535@gmail.com>
2025-03-18 11:55:04 +0600

    Added child class 'Student'

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -4,25 +4,56 @@ namespace OOP_C_
 {
 
 
-    public class Student
+    abstract class Person
     {
-        private string name;
-        private string rollNumber;
-        private DateTime dateOfBirth;
-         
-        public Student(string name, string rollNumber, DateTime dateOfBirth)
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public int ID { get; set; }
+
+
+        public Person(string name, string age, int id)
         {
-            this.name = name;
-            this.rollNumber = rollNumber;
-            this.dateOfBirth = dateOfBirth;
+            Name = name;
+            Age = age;
+            ID = id;
         }
+
+        public abstract void GetDetails();
         
     }
+
+     class Student : Person
+    {
+
+        public string RollNumber { get; set; }
+        public string Department { get; set; }
+        public double CGPA { get; set; }
+
+
+        public Student(string name, string age,int id,string rollNumber, string department, double cGPA)
+               : base(name,age,id)     // Student Constructor calling Person Constructor
+        {
+            RollNumber = rollNumber;
+            Department = department;
+            CGPA = cGPA;
+        }
+
+       
+        public override void GetDetails()
+        {
+            Console.WriteLine($"Name: {Name}\nAge: {Age}\nRoll: {RollNumber}\nDepartment: {Department}\nCGPA: {CGPA}");
+        }
+
+    }
+
+
     public class Myclass
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
+        {   
+
+            Student student1 = new Student("Al Momen","24",1234,"CS210","CSE",3.50);
+            student1.GetDetails();
         }
     }
 }
