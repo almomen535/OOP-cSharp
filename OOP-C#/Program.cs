76fb72c7c068f13a6a7aceb247a40e0469cc182a
Al Momen <almomen535@gmail.com>
2025-03-24 10:51:45 +0600

    added a class

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -1,5 +1,15 @@
 ﻿namespace OOP_C_
-{
+{     
+
+
+    abstract class Team
+    {
+        public string Name { get; set; }
+
+        abstract string GetDetalis();
+      
+    }
+
     internal class Program
     {
         static void Main(string[] args)
