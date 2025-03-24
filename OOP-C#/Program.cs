3ca8f5e99524b5648a32d60ab6df0f182d99ae1b
Al Momen <almomen535@gmail.com>
2025-03-24 11:50:22 +0600

    added mangement class

--- OOP-C#/Program.cs
+++ OOP-C#/Program.cs
@@ -2,49 +2,53 @@
 {
 
 
-    abstract class Team
+    abstract class Sports
     {
-        public string Name { get; set; }
-        public string ID { get; set; }
+        public string TeamName { get; set; }
 
-        public abstract string GetDetalis();
+        public abstract string DisplayInfo();
 
     }
 
-   class Teams : Team
+   class Football : Sports
     {
         public string Owner { get; set; }
         public string Captain { get; set; }
 
 
-        public Teams(string name,string id, string owner, string captain)
+        public Football(string name, string owner, string captain)
         {
 
-            Name = name;
-            ID = id;
+            TeamName = name;
+          
             Owner = owner;
             Captain = captain;
         }
 
-        public override string GetDetalis()
+        public override string DisplayInfo()
         {
-            return ($"Team Name: {Name},  ID: {ID},  Owner: {Owner}, Captain: {Captain}");
+            return ($"Team Name: {TeamName}, Owner: {Owner}, Captain: {Captain}");
         }
 
 
     }
 
     
+    public class Management
+    {
+       
+    }
     
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Teams team1 = new Teams("FC Hexclan", "F153","Sayeduzzaman Islam","Al Momen");
+            Football team1 = new Football("FC Hexclan", "Sayeduzzaman Islam","Al Momen");
+            Football team2 = new Football("Thread Titans", "Tanvir Islam", "Asif Mahmud");
 
 
-            Console.WriteLine(team1.GetDetalis());
+            Console.WriteLine(team1.DisplayInfo());
         }
     }
 }
